5a641bb6c54e489caaed96ce13c87445278d3441
Rich <rich@freebirdrides.com>
2021-02-13 22:30:41 -0800

    cleanup storybook

--- src/components/Image/index.stories.js
+++ src/components/Image/index.stories.js
@@ -7,7 +7,7 @@ export default {
   component: Image
 }
 
-const Template = (args) => <Image {...args} />
+const Template = args => <Image {...args} />
 
 export const Default = Template.bind({})
 Default.args = {
@@ -33,16 +33,16 @@ Border.args = {
   border: true
 }
 
-export const WMissingImage = Template.bind({})
-WMissingImage.args = {
-  src: 'brokenimageurl.com',
-  alt: 'kitty'
-}
+// export const WMissingImage = Template.bind({})
+// WMissingImage.args = {
+//   src: 'brokenimageurl.com',
+//   alt: 'kitty'
+// }
 
-export const WAltImagePropAndBrokenImage = Template.bind({})
-WAltImagePropAndBrokenImage.args = {
-  src:
-    'https://s-media-cache-ak0.pinimg.com/originals/6d/96/41/6d9641691af89520f3b19b26bca4b5fc.jpg',
-  alt: 'kitty',
-  altImage: true
-}
+// export const WAltImagePropAndBrokenImage = Template.bind({})
+// WAltImagePropAndBrokenImage.args = {
+//   src:
+//     'https://s-media-cache-ak0.pinimg.com/originals/6d/96/41/6d9641691af89520f3b19b26bca4b5fc.jpg',
+//   alt: 'kitty',
+//   altImage: true
+// }
--- src/views/ListView/style.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components'
-import { space } from 'styled-system'
-
-export default styled.div`
-  ${space};
-`
